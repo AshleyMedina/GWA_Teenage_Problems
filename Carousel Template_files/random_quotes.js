782c775208dc80c3a2ab48998661e2f0cb5f419d
Kelly Mack <kellyrmc2012@gmail.com>
2016-07-26 11:19:54 -0700

    fixed file names and added all

---
+++ Carousel Template_files/random_quotes.js
@@ -0,0 +1,47 @@
+<body> 
+[...] 
+<span id="Quotes_Sad">Good morning</span> 
+[...] 
+
+<script type="text/javascript"> 
+today=new Date() 
+if(today.getSeconds() >= 0 && today.getSeconds() < 30) 
+{      
+document.getElementById('True happiness is... to enjoy the present, without anxious dependence upon the future.
+			</br>―  Lucius Annaeus Seneca</br>').innerHTML='Remember that the happiest people are not those getting more, but those giving more.
+			</br>―  H. Jackson Brown, Jr.</br>'; 
+} 
+else 
+{      
+document.getElementById('Remember that the happiest people are not those getting more, but those giving more. 
+			</br>―  H. Jackson Brown, Jr.</br>').innerHTML='Happiness is not a matter of intensity but of balance, order, rhythm and harmony. 
+			</br>―  Thomas Merton</br>'; 
+else 
+{      
+document.getElementById('Happiness is not a matter of intensity but of balance, order, rhythm and harmony.” 
+			</br>―  Thomas Merton</br>').innerHTML='If you want happiness for an hour — take a nap.
+		If you want happiness for a day — go fishing.
+		If you want happiness for a year — inherit a fortune.
+		If you want happiness for a lifetime — help someone else. 
+			</br>― Chinese Proverbs</br>'; 
+} 
+else 
+{      
+document.getElementById('If you want happiness for an hour — take a nap.
+		If you want happiness for a day — go fishing.
+		If you want happiness for a year — inherit a fortune.
+		If you want happiness for a lifetime — help someone else. 
+			</br>― Chinese Proverbs</br>').innerHTML='Happy people plan actions, they don’t plan results. 
+			</br>― Dennis Waitley</br>'; 
+} 
+else 
+{      
+document.getElementById('Happy people plan actions, they don’t plan results. 
+			</br>― Dennis Waitley</br>').innerHTML='Happiness is not in the mere possession of money; it lies in the joy of achievement, in the thrill of creative effort.
+			</br>― Franklin D. Roosevelt</br>'; 
+} 
+} 
+</script> 
+
+[...] 
+</body> 
